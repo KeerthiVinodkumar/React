f268812ca6ba7956c183af53b794014b38835c0e
KeerthiVinodkumar <vinodkumarkeerthi725@gmail.com>
2023-11-08 14:24:13 +0530

    building a Food Delivery App

--- App.js
+++ App.js
@@ -1,31 +1,87 @@
-// <div>
-{
-  /* <div id=parent> 
-  <div id=child>
-  <h1>this is h1</h1>
-  <h2>this is h2</h2>
-  <div>
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-</div> */
-}
+// Header
+// -Logo
+// -logo items
+// Body
+// -Search bar
+// -cards
+//    -Ratings
+//     -cuisines
 
-{
-  /* <div id="heading">
-  <h1>Hello world new to react</h1>
-</div> */
-}
-
-// const heading= React.createElement("h1", {}, "Hello world to react")
-// const root = ReactDOM.createRoot(document.getElementById("root"))
-// root.render(heading)
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" },
-  [
-    React.createElement("h1", {}, "i am an h1 tag"),
-    React.createElement("h2", {}, "i am an h2 tag"),
-  ]),
-  React.createElement("div", { id: "child1" },
-  [React.createElement("h1", {}, "this is child1 tag")]),
-]);
+const Applist = () => {
+  return (
+    <div>
+      <Header />
+      <Body />
+    </div>
+  );
+};
+const Header = () => {
+  return (
+    <div className="res_Header">
+      <div className="logo">
+        <img src="https://static.vecteezy.com/system/resources/previews/008/687/818/original/food-delivery-logo-free-vector.jpg"></img>
+      </div>
+      <div className="res_items">
+        <ul>
+          <li>Home</li>
+          <li>AboutUs</li>
+          <li>contactUs</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+const ReastaurantCard = () => {
+  return (
+    <div className="res_card">
+      <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/p0d9zl2iw9ovjqnwinq4"></img>
+      <h2>Naidu Hotal </h2>
+      <h6>Rating-4.2</h6>
+      <h5>Souch Indian Home Food Anantapur</h5>
+    </div>
+  );
+};
+const Body = () => {
+  return (
+    <div className="res_body">
+      <div className="search_bar">
+        <button>serach</button>
+      </div>
+      <div className="restourant_Cards">
+        <ReastaurantCard />
+        <ReastaurantCard />
+        <ReastaurantCard />
+        <ReastaurantCard />
+        <ReastaurantCard />
+        <ReastaurantCard />
+        <ReastaurantCard />
+        <ReastaurantCard />
+      </div>
+    </div>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<Applist />);
+
+// const Title =()=>{
+//  return <div>
+//     <h1>this is Title componenht</h1>
+
+//   </div>
+// };
+// const number = 100
+// const Heading3 = () => {
+//   return (
+//     <div id="container">
+
+//       <h1>hi this is functional component</h1>
+//       <h2>this is another functional component</h2>
+//       <Title/>
+//     </div>
+//   );
+// };
+// root.render(<Heading3/>);
