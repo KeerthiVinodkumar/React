95e7b54ba94e21563824673af1df3f82290d0c9a
KeerthiVinodkumar <vinodkumarkeerthi725@gmail.com>
2023-11-13 12:21:52 +0530

    fetched Swiggy live api

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,16 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ReastaurantCard from "./Reastaurantcard";
-import resList from "../utils/mockData";
 
 const Body = () => {
-  const [listedItems, setlistedItems] = useState(resList);
+  const [listedItems, setlistedItems] = useState([]);
+  useEffect(() => {
+    fetchdata();
+  }, []);
+
+  const fetchdata = async() => {
+    const data =await fetch(
+      "https://www.swiggy.com/mapi/homepage/getCards?lat=14.678709664474182&lng=77.58391968905926"
+    );
+    const json=await data.json()
+    console.log(json);
+   const items = json.data.success.cards[1].gridWidget.gridElements.infoWithStyle.restaurants.map(restaurant => restaurant) || [];
+   setlistedItems(items)
+   console.log(items);
+  };
+
+  console.log("body rendered");
   return (
     <div className="res_body">
       <div className="search_bar">
-        <button className="filter_btn"
+        <button
+          className="filter_btn"
           onClick={() => {
             const filterdlist = listedItems.filter(
-              (res) => res.restaurants.info.avgRatingString > 4
+              (res) => res.restaurants.avgRatingString > 4
             );
             setlistedItems(filterdlist);
           }}
@@ -21,7 +37,7 @@ const Body = () => {
       <div className="restourant_Cards">
         {listedItems.map((listedItems) => (
           <ReastaurantCard
-            key={listedItems.restaurants.info.id}
+            key={listedItems.id}
             resdata={listedItems}
           />
         ))}
