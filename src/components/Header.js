4a31fff150198f85687db53b06f3199e99723917
KeerthiVinodkumar <vinodkumarkeerthi725@gmail.com>
2023-11-13 21:32:43 +0530

    built a login,logout button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,30 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/contants";
 const Header = () => {
-    return (
-      <div className="res_Header">
-        <div className="logo">
-          <img src={LOGO_URL}></img>
-        </div>
-        <div className="res_items">
-          <ul>
-            <li>Home</li>
-            <li>AboutUs</li>
-            <li>contactUs</li>
-            <li>Cart</li>
-          </ul>
-        </div>
+  const [button, setbutton] = useState("login");
+  console.log("rerendered");
+  return (
+    <div className="res_Header">
+      <div className="logo">
+        <img src={LOGO_URL}></img>
       </div>
-    );
-  };
-  export default Header
+      <div className="res_items">
+        <ul>
+          <li>Home</li>
+          <li>AboutUs</li>
+          <li>contactUs</li>
+          <li>Cart</li>
+          <button
+            className="login_btn"
+            onClick={() => {
+              button === "login" ? setbutton("logout") : setbutton("login");
+            }}
+          >
+            {button}
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
+};
+export default Header;
