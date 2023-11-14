890d85a505e30553ee12f096ad5e253f5c387a9f
KeerthiVinodkumar <vinodkumarkeerthi725@gmail.com>
2023-11-14 16:40:22 +0530

    used_react-router

--- src/App.js
+++ src/App.js
@@ -2,16 +2,43 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import { RouterProvider, createBrowserRouter,Outlet } from "react-router-dom";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
 const Applist = () => {
   return (
     <div>
       <Header />
-      <Body />
+     <Outlet/>
     </div>
   );
 };
 
+const approuter=createBrowserRouter([
+  {
+    path:"/",
+    element:<Applist/>,
+    children:[
+      {
+      path:"/",
+      element:<Body/>
+      },
+      {
+        path:"/about",
+      element:<About/>
+      },
+      {
+        path:"/contact",
+        element:<Contact/>
+      }
+    ],
+    errorElement:<Error/>
+  },
+ 
+])
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Applist />);
+root.render(<RouterProvider router={approuter} />);
 export default Applist
 
