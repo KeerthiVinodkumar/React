260b8382ea32b88cbb9622e0d94fb170f30d544b
KeerthiVinodkumar <vinodkumarkeerthi725@gmail.com>
2023-11-21 13:21:04 +0530

    stored swiggy api to a variable

--- src/utils/contants.js
+++ src/utils/contants.js
@@ -2,3 +2,5 @@ export const CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 export const LOGO_URL =
   "https://static.vecteezy.com/system/resources/previews/008/687/818/original/food-delivery-logo-free-vector.jpg";
+export const Menu_API =
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=14.678709664474182&lng=77.58391968905926&restaurantId=";
