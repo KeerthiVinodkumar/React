6a4df91865591efbbdfd7e95684114d5964cdbf7
KeerthiVinodkumar <vinodkumarkeerthi725@gmail.com>
2023-11-21 13:18:29 +0530

    built a card to About us page

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import User from "./User"
+import Userclass from "./Userclass"
+const About =()=>{
+    return(
+        <div><User/>
+            <Userclass/>
+        </div>
+        
+    )
+}
+export default About
---
+++ src/components/User.js
@@ -0,0 +1,10 @@
+const User = ()=>{
+    return(
+        <div className="user_card">
+ <h1>Name:Vinod</h1>
+ <h2>Location:Anantapur</h2>
+ <h3>age:23</h3>
+        </div>
+    )
+}
+export default User
