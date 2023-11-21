839847c80543c3987640a57b42592ae4a12aa83c
KeerthiVinodkumar <vinodkumarkeerthi725@gmail.com>
2023-11-21 23:46:13 +0530

    life_Cycle_Methods

--- src/components/Userclass.js
+++ src/components/Userclass.js
@@ -1,15 +1,65 @@
-import React from "react"
+import React from "react";
 
-class Userclass extends React.Component{
+class Userclass extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      userinfo:{
+        login:"dummy"
+      }
+    };
+   console.log(this.props.name + "child constructor");
+  }
+  async componentDidMount() {
+   console.log(this.props.name + "child componentdidmouont");
+ 
+     const data = await fetch("https://api.github.com/users/keerthivinodkumar")
+     const json = await data.json()
+
+     this.setState({
+        userinfo:json
+     })
+     console.log(json);
+  }
+  componentDidUpdate(prevProps,prevState){
+    if(this.state.userinfo!==prevState.userinfo){
+      const data =    fetch(this.state.userinfo)
+      console.log(data);
+    }
+    
+
+  }
+  componentWillUnmount(){
+    console.log("componentwillunmount");
+  }
   render() {
-    return(
-        <div className="user_card">
-        <h1>Name:Vinod</h1>
+    console.log(this.props.name + "child render");
+  
+    const {login,avatar_url}= this.state.userinfo
+   
+    return (
+
+      <div className="user_card">
+    
+        <img src={avatar_url}></img>
+        <h1>Name:{login}</h1>
         <h2>Location:Anantapur</h2>
         <h3>age:23</h3>
-               </div>
-    )
+      </div>
+    );
   }
-
 }
-export default Userclass
+export default Userclass;
+
+
+
+/*
+* constructor(dummy)
+* Render(dummy)
+* child userclass
+*
+*
+*
+
+*/
