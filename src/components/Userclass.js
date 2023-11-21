b1201d0af2c9d7f121b8f1f4d839ab611e52cc56
KeerthiVinodkumar <vinodkumarkeerthi725@gmail.com>
2023-11-21 13:17:07 +0530

    built class based component

---
+++ src/components/Userclass.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+class Userclass extends React.Component{
+  render() {
+    return(
+        <div className="user_card">
+        <h1>Name:Vinod</h1>
+        <h2>Location:Anantapur</h2>
+        <h3>age:23</h3>
+               </div>
+    )
+  }
+
+}
+export default Userclass
