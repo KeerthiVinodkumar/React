084e67e86562a22c8e5924e13cc837dffb927e16
KeerthiVinodkumar <vinodkumarkeerthi725@gmail.com>
2023-11-26 23:32:12 +0530

    Tailwind

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,26 @@
-import {  useState } from "react";
+import {  useEffect, useState } from "react";
 import { LOGO_URL } from "../utils/contants";
 import { Link } from "react-router-dom";
-const Header = () => {
-  const [button, setbutton] = useState("login");
+import useOnlineStatus from "../utils/useOlineStatus";
 
+const Header = () => {
+ 
+  const [button, setbutton] = useState("login");
+ 
+
+  const onlinestatus = useOnlineStatus()
   return (
-    <div className="res_Header">
-      <div className="logo">
+    <div className="flex">
+      <div className="w-36">
         <img src={LOGO_URL}></img>
       </div>
-      <div className="res_items">
-        <ul>
+      <div className="flex">
+        <ul className="flex p-4 m-4"  >
+          <li>Onlinestatus:{onlinestatus? "✔":"🔴"}</li>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/about">AboutUs</Link></li>
           <li><Link to="/contact" >contactUs</Link></li>
+          <li><Link to={"/grocery"}>Grocery</Link></li>
           <li>Cart</li>
           <button
             className="login_btn"
